7b6726eea111be74e6717132060565ed8e8c2130
yuseogi0218 <yuseogi0218@gmail.com>
2024-12-24 15:26:10 +0900

    FEAT - JPA Auditing

--- src/main/java/hanghae99/reboot/notification/NotificationApplication.java
+++ src/main/java/hanghae99/reboot/notification/NotificationApplication.java
@@ -2,7 +2,9 @@ package hanghae99.reboot.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NotificationApplication {
 
